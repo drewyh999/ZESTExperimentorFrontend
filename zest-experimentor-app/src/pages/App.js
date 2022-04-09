be9b458e82909c238cae4e1d635a19b156474463
everroc <drewyh1999@outlook.com>
2022-04-09 12:07:17 +0200

    Init

---
+++ zest-experimentor-app/src/pages/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Admin from './components/admin.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Admin/>
+    </div>
+  );
+}
+
+export default App;
---
+++ zest-experimentor-app/src/pages/components/admin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Admin extends React.Component {
+	render (){
+		return(
+			<div>
+				<h1>
+				This is the admin component
+				</h1>
+			</div>
+
+		);}
+}
+
+export default Admin;
